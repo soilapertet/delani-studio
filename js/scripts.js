57a63c4c6617001366fa4aa25ae72fd4f724f63a
Soila Pertet <nicolepertet@gmail.com>
2021-12-19 10:54:39 +0300

    Create two arrays in the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+var tasks = ["design", "development", "product-management"];
+var descriptions = ["design-info", "development-info", "product-info"];
+
 $(document).ready(function(){
   $("#design").click(function(){
     $("#design").slideToggle();
