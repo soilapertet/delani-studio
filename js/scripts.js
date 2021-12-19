19b57ae41699a7d8ec1199761a9f44105e6fe4dd
Soila Pertet <nicolepertet@gmail.com>
2021-12-19 16:33:38 +0300

    Create a function which toggles between the icon and the description for the remaining two icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 var tasks = ["design", "development", "product-management"];
 var descriptions = ["design-info", "development-info", "product-info"];
 
@@ -10,4 +11,21 @@ $(document).ready(function(){
     $("#design-info").slideToggle();
     $("#design").slideToggle();
   });
-})
+  $("#development").click(function(){
+    $("#development").slideToggle();
+    $("#development-info").slideToggle();
+  });
+  $("#development-info").click(function(){
+    $("#development-info").slideToggle();
+    $("#development").slideToggle();
+  });
+  $("#product-management").click(function(){
+    $("#product-management").slideToggle();
+    $("#product-info").slideToggle();
+  });
+  $("#product-info").click(function(){
+    $("#product-info").slideToggle();
+    $("#product-management").slideToggle();
+  });
+});
+
